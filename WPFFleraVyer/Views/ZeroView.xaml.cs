1ca8c51dae8b9757e2f993cd1fa8bd3164afa8b2
niklas-hjelm <niklas.hjelm@iths.se>
2023-10-30 11:01:42 +0100

    added views

---
+++ WPFFleraVyer/Views/ZeroView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFFleraVyer.Managers;
+
+namespace WPFFleraVyer.Views
+{
+    /// <summary>
+    /// Interaction logic for ZeroView.xaml
+    /// </summary>
+    public partial class ZeroView : UserControl
+    {
+        public ZeroView()
+        {
+            InitializeComponent();
+        }
+
+        private void ZeroBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            CounterManager.CounterModel.Counter = 0;
+        }
+    }
+}
