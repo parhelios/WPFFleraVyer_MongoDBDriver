361d1dc8a2e34137bbb9ceb475683fef8e5cc629
niklas-hjelm <niklas.hjelm@iths.se>
2023-10-30 09:58:26 +0100

    Add project files.

---
+++ WPFFleraVyer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFFleraVyer.Models;
+
+namespace WPFFleraVyer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public CounterModel CounterModel { get; set; } = new CounterModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DecreaseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            CounterModel.Counter--;
+            CounterText.Text = CounterModel.Counter.ToString();
+        }
+
+        private void ZeroBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            CounterModel.Counter = 0;
+            CounterText.Text = CounterModel.Counter.ToString();
+        }
+
+        private void IncreaseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            CounterModel.Counter++;
+            CounterText.Text = CounterModel.Counter.ToString();
+        }
+    }
+}
---
+++ WPFFleraVyer/Models/CounterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WPFFleraVyer.Models;
+
+public class CounterModel
+{
+    private int _counter;
+
+    public int Counter
+    {
+        get { return _counter; }
+        set { _counter = value; }
+    }
+}
