b156ce13c231ac32f3f92e01c690217a39f6bb01
parhelios <143404279+parhelios@users.noreply.github.com>
2024-01-15 10:20:11 +0100

    -

--- DataAccess/Services/PeopleRepository.cs
+++ DataAccess/Services/PeopleRepository.cs
@@ -12,10 +12,25 @@ public class PeopleRepository
         var hostName = "localhost";
         var port = "27017";
         var databaseName = "PeopleDb";
+
         var client = new MongoClient($"mongodb://{hostName}:{port}");
         var database = client.GetDatabase(databaseName);
-        _people = database.GetCollection<Person>("People", new MongoCollectionSettings() { AssignIdOnInsert = true });
+        _people = database.GetCollection<Person>("People", new MongoCollectionSettings { AssignIdOnInsert = true });
     }
 
+    public string AddPerson(string firstName, string lastName)
+    {
+        var newPerson = new Person()
+        {
+            FirstName = firstName,
+            LastName = lastName
+        };
+        _people.InsertOne(newPerson);
+
+        var filter = Builders<Person>.Filter.Eq("FirstName", firstName);
+        var personSaved = _people.Find(filter).FirstOrDefault();
+
+        return personSaved.Id.ToString();
+    }
 
 }
