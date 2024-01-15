ee74cd6a8defc6e0990072424b581b382888578e
parhelios <143404279+parhelios@users.noreply.github.com>
2024-01-15 13:57:45 +0100

    -

--- DataAccess/Enteties/Person.cs
+++ DataAccess/Enteties/Person.cs
@@ -1,4 +1,5 @@
-﻿using Common.Enums;
+﻿using System.Runtime.CompilerServices;
+using Common.Enums;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -9,6 +10,11 @@ public class Person
     public ObjectId Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    [BsonRepresentation(BsonType.String)]
-    public HairColor HairColor { get; set; }
+    [BsonRepresentation(BsonType.String)] public HairColor HairColor { get; set; }
+
+    public List<Pet> Pets { get; set; } = new()
+    {
+        new Pet() { Name = "Babe", AnimalType = AnimalType.Piglet },
+        new Pet() { Name = "Zoe", AnimalType = AnimalType.Dog }
+    };
 }
