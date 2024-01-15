21c76bc40154167b4cb5d82b17aabcadc2fc6ce0
parhelios <143404279+parhelios@users.noreply.github.com>
2024-01-15 09:54:24 +0100

    -

---
+++ DataAccess/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+
+namespace DataAccess.Entities;
+
+public class Person
+{
+    public ObjectId Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+
+}
---
+++ DataAccess/Services/PeopleRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Entities;
+using MongoDB.Driver;
+
+namespace DataAccess.Services;
+
+public class PeopleRepository
+{
+    private readonly IMongoCollection<Person> _people;
+
+    public PeopleRepository()
+    {
+        var hostName = "localhost";
+        var port = "27017";
+        var databaseName = "PeopleDb";
+
+        var client = new MongoClient($"mongodb://{hostName}:{port}");
+        var database = client.GetDatabase(databaseName);
+        _people = database.GetCollection<Person>("People", new MongoCollectionSettings { AssignIdOnInsert = true });
+    }
+
+
+}
